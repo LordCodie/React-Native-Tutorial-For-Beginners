264d070b70c11ce9df120c2053d2ddc22e0924bd
Bafana Baloyi <baloyibafana177@Gmail.com>
2025-08-07 17:08:32 +0200

    Part 1

---
+++ CoreComponents/App.js
@@ -0,0 +1,62 @@
+import {
+  View, Text,
+  Image, ImageBackground,
+  ScrollView, Button,
+  Pressable, Modal,
+  StatusBar, ActivityIndicator,
+  Alert, StyleSheet
+} from "react-native";
+const logoImg = require("./assets/adaptive-icon.png")
+
+export default function App() {
+  const { container, title, lightblueBg, lightgreenBg, box, androidShadow } = styles
+  return (
+    <View style={container}>
+
+      <View style={[box,lightblueBg]}>
+        <Text>Lightblue box</Text>
+      </View>
+
+      <View style={[box,lightgreenBg, androidShadow]}>
+        <Text>Lightgreen box</Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'plum',
+    padding: 60
+  },
+  title: {},
+  box: {
+    width: 250,
+    height: 250,
+    // padding: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    marginVertical: 10,
+    borderWidth: 2,
+    borderColor: "purple",
+  },
+  lightblueBg: {
+    backgroundColor: "lightblue"
+  },
+  lightgreenBg: {
+    backgroundColor: "lightgreen"
+  },
+  boxShadow: {
+    shadowColor: '#333333',
+    shadowOffset: {
+      width: 6,
+      height: 6
+    },
+    shadowOpacity: 0.6,
+    shadowRadius: 4
+  },
+  androidShadow: {
+    elevation: 10
+  }
+})
