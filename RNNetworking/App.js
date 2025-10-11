9faaab87ee000275aa432b3544f421ab1c8dbeba
Bafana Baloyi <153275673+LordCodie@users.noreply.github.com>
2025-10-11 09:53:52 +0000

    fg

--- RNNetworking/App.js
+++ RNNetworking/App.js
@@ -1,31 +1,38 @@
-import { 
-  View, StyleSheet, StatusBar, 
-  FlatList, Text, ActivityIndicator 
+import {
+  View, StyleSheet, StatusBar,
+  FlatList, Text, ActivityIndicator
 } from 'react-native';
 import { useState, useEffect } from 'react';
 
 export default function App() {
-
   const [postList, setPostList] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState("")
 
   const fetchData = async (limit = 10) => {
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
-    )
-    const data = await response.json()
-    setPostList(data)
-    setIsLoading(false)
+    try {
+      const response = await fetch(
+        `https://jsonplaceholderasd.typicode.com/posts?_limit=${limit}`
+      )
+      const data = await response.json()
+      setPostList(data)
+      setIsLoading(false)
+      setError(false)
+    } catch (error) {
+      console.error(`Error fetching data: ${error}`)
+      setIsLoading(false)
+      setError("Falied to fetch post list")
+    }
   }
 
   useEffect(() => {
     fetchData()
   }, [])
 
-  if (isLoading){
+  if (isLoading) {
     return (
       <View style={styles.loadingContainer}>
-        <ActivityIndicator size={'large'} color={'blue'}/> 
+        <ActivityIndicator size={'large'} color={'blue'} />
         <Text>Loading...</Text>
       </View>
     )
@@ -33,17 +40,24 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.listContainer}>
-        <FlatList
-          data={postList}
-          renderItem={({ item }) => (
-            <View style={styles.card}>
-              <Text style={styles.titleText}>{item.title}</Text>
-              <Text style={styles.bodyText}>{item.body}</Text>
-            </View>
-          )}
-        />
-      </View>
+      {error ? (
+        <View style={styles.errorContainer}>
+          <Text style={styles.errorText}>{error}</Text>
+        </View>
+      ) :
+        <>
+          <View style={styles.listContainer}>
+            <FlatList
+              data={postList}
+              renderItem={({ item }) => (
+                <View style={styles.card}>
+                  <Text style={styles.titleText}>{item.title}</Text>
+                  <Text style={styles.bodyText}>{item.body}</Text>
+                </View>
+              )}
+            />
+          </View>
+        </>}
     </View>
   );
 }
@@ -77,5 +91,18 @@ const styles = StyleSheet.create({
     paddingTop: StatusBar.currentHeight,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  errorContainer: {
+    backgroundColor: "#FFC0CB",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    margin: 16,
+    alignItems: "center"
+  },
+  errorText: {
+    color: "#DB000C",
+    fontSize: 16,
+    textAlign: "center"
   }
 });
